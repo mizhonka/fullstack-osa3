44003a6ed3956fa462493dfd08f70c7db98c4dcb
Miranda Honkanen <miranda.honkanen@helsinki.fi>
2024-01-31 15:55:31 +0200

    3.1

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const app = express()
+
+const notes=[
+    {
+        id: 1,
+        name: 'Arto Hellas',
+        number: '040-123456'
+    },
+    {
+        id: 2,
+        name: 'Ada Lovelace',
+        number: '39-44-5323523'
+    },
+    {
+        id: 3,
+        name: 'Dan Abramov',
+        number: '12-43-234345'
+    },
+    {
+        id: 4,
+        name: 'Mary Poppendick',
+        number: '39-23-6423122'
+    }
+]
+
+app.get('/api/persons', (request, response)=>{
+    response.json(notes)
+})
+
+const port=3001
+app.listen(port)
