7aa35ed9fe76130ffdfface0760ae7d6467bb321
Miranda Honkanen <miranda.honkanen@helsinki.fi>
2024-02-06 17:09:56 +0200

    build

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ morgan.token('data', (req)=>{JSON.stringify(req.body)})
 
 app.use(cors())
 app.use(express.json())
+app.use(express.static('dist'))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'))
 
 let notes=[
