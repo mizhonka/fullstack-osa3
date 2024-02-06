d2af40a4ed206d02597ea3e221f1473bc969a3f4
Miranda Honkanen <miranda.honkanen@helsinki.fi>
2024-02-06 16:50:59 +0200

    3.9

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express')
+const cors=require('cors')
 const morgan = require('morgan')
 const app = express()
 
 morgan.token('data', (req)=>{JSON.stringify(req.body)})
 
+app.use(cors())
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'))
 
@@ -88,5 +90,5 @@ app.delete('/api/persons/:id', (request, response)=>{
     response.status(204).end()
 })
 
-const port=3001
+const port=process.env.port || 3001
 app.listen(port)
