3d19250fea8bdebe9d56cd770a7259de12d4a8ed
Miranda Honkanen <miranda.honkanen@helsinki.fi>
2024-02-06 14:58:12 +0200

    3.5

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let notes=[
     {
         id: 1,
@@ -43,6 +45,25 @@ app.get('/api/persons/:id', (request, response)=>{
     else{
         response.status(404).end()
     }
+})
+
+const generateId=()=>{return Math.floor(Math.random()*100)}
+
+app.post('/api/persons', (request, response) =>{
+    const body=request.body
+    if(!body.name && !body.number){
+        return response.status(400).json({
+            error: 'content missing'
+        })
+    }
+
+    const person={
+        id: generateId(),
+        name: body.name,
+        number: body.number
+    }
+    notes=notes.concat(person)
+    response.json(person)
 })
 
 app.delete('/api/persons/:id', (request, response)=>{
